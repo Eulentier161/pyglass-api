f3e4aa7be65eca51b6d3d0cf50d927209ae4347c
Eulentier161 <git@eule.wtf>
2023-12-01 16:24:36 +0100

    pottery

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="pyglass-api",
-    version="0.2.0",
-    packages=find_packages(include=["pyglass", "pyglass.*"]),
-    install_requires=["requests", "certifi", "charset-normalizer", "idna", "urllib3"],
-)
